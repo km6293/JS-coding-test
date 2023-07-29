b3c8e7509d13a2fa464d9d1399827da76dde0339
km6293 <alstn7616@naver.com>
2023-07-30 08:56:53 +0900

    feat : [PGS] 요격 시스템 풀이

--- MinsuKim/week_01/[PGS]요격 시스템.js
+++ MinsuKim/week_01/[PGS]요격 시스템.js
@@ -1,1 +1,26 @@
-console.log(1)
+function solution(a) {
+  let b = a.sort((a,b) => {
+    if(a[0] < b[0]){
+      return -1;
+    }else if(a[0] === b[0]){
+      return a[1] < b[1] ? -1 : 1;
+    }else{
+      return 1;
+    }
+  })
+
+  let cnt = 1;
+  let [s,e] = b.shift();
+
+  b.forEach(r => {
+    if(r[0] >= e){
+      cnt += 1;
+      [s,e] = r;
+    }else{
+      if(r[0] > s) s = r[0];
+      if(r[1] < e) e = r[1];
+    }
+  })
+
+  return cnt
+}
