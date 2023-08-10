0238f8e36bf5f860105e274cf503dbc8cd180f8e
km6293 <alstn7616@naver.com>
2023-08-10 10:56:04 +0900

    feat : [BOJ] (2606) 바이러스 진행 완료

--- MinsuKim/week_03/[BOJ]2606_바이러스.js
+++ MinsuKim/week_03/[BOJ]2606_바이러스.js
@@ -1,9 +1,28 @@
-const arr = new Array(3).fill([]); 
-console.log(arr)
-let b = arr.map(e => e);
-b[0].push(1);
-console.log(b)
+// let [total, pair, ...arr] = require('fs').readFileSync(__dirname + "/../input.txt").toString().trim().split("\n");
+let [total, couple, ...arr] = require('fs').readFileSync("/dev/stdin").toString().trim().split("\n");
+total = parseInt(total);
 
-let test = Array.from([1,2,3'], (e) => {[]});
-// test[0].push(1);
-console.log(test)
+let graph = Array.from({length:total+1}, () => [])
+let visited = Array.from({length:total+1}, () => false);
+visited[1] = true;
+let cnt = 0;
+
+arr.forEach(e => {
+  const [a, b] = e.split(' ').map(Number);
+  graph[a].push(b);
+  graph[b].push(a);
+})
+
+function chk(arr){
+  arr.forEach(e => {
+    if(!visited[e]){
+      visited[e] = true;
+      cnt += 1;
+      chk(graph[e]);
+    }
+  })
+}
+
+chk(graph[1]);
+console.log(cnt);
+
